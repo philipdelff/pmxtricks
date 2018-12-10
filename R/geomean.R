32c2220c3697a589f3583745e1813ae60b21c1a9
Delff <kctw748@astrazeneca.net>
2018-12-09 20:13:59 -0500

    Tiny correction to geomean Signed-off-by: Delff <kctw748@astrazeneca.net>

--- R/geomean.R
+++ R/geomean.R
@@ -3,7 +3,7 @@
 ##' @param x vector to calculate the geometric mean of
 ##' @param z.rm removes zeros before calculation?
 
-geomean <- function(x,log=F,z.rm=F) {
+geomean <- function(x,z.rm=F) {
     if(z.rm) x <- x[x!=0]
     exp(mean(log(x)))
 }
