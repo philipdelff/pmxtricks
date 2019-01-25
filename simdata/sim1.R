8b7ee19a5b593ff2d8fbe7ba696f8e040fac1c1d
Philip Delff <philip@delff.dk>
2019-01-24 22:24:25 -0500

    simulation work + went to devtools NAMESPACE creation

--- simdata/sim1.R
+++ simdata/sim1.R
@@ -1,4 +1,8 @@
 ###{ Section start: Initialisation
+library(devtools)
+## install.packages("PKPDsim")
+## install_github("InsightRX/PKPDsim")
+
 library(PKPDsim)
 library(ggplot2)
 ###} Section end
@@ -6,7 +10,7 @@ library(ggplot2)
 
 
 ###{ Section start: Simulate covariates
-nSubjs <- 82
+nSubjs <- 4
 df.cov <- data.frame(ID=sprintf("%03d",1:nSubjs))
 
 
@@ -24,19 +28,37 @@ df.cov <- data.frame(ID=sprintf("%03d",1:nSubjs))
 
 
 
-tvpars=data.frame(KA1=1,KT1=3,CL=.25,KM=5,VM=5,VC=1,VP=10,Q=0.15)
+tvpars=data.frame(KA1=1,KT1=3,CL=1,KM=5,VM=0,VC=10,VP=1,Q=.8)
 
-diag.omega <- rep(.2,ncol(tvpars))
+### covariance of omega
+## diag.omega <- rep(.5,ncol(tvpars))
+## diag.omega[1:3] <- c(.3,.2,1)
+## omega <- diag(diag.omega)
+## rownames(omega) <- colnames(tvpars)
+## colnames(omega) <- colnames(tvpars)
+## omega
+## omega[2,1] <- omega[1,2] <- .3/2   ## ka kt
+## omega[3,6] <- omega[6,3] <- .1     ## CL VC
+## omega[3,8] <- omega[8,3] <- .1     ## CL Q
+## omega[6,7] <- omega[7,6] <- .3/2   ## VP VC
+## omega
+
+## easier to define marginal vars and correlation, then derive covariance.
+diag.omega <- rep(.5,ncol(tvpars))
 diag.omega[1:3] <- c(.3,.2,1)
-omega <- diag(diag.omega)
-rownames(omega) <- colnames(tvpars)
-colnames(omega) <- colnames(tvpars)
-omega
-omega[2,1] <- omega[1,2] <- .3/2   ## ka kt
-omega[3,6] <- omega[6,3] <- .1     ## CL VC
-omega[3,8] <- omega[8,3] <- .1     ## CL Q
-omega[6,7] <- omega[7,6] <- .3/2   ## VP VC
-omega
+
+ro.omega <- diag(rep(1,length(diag.omega)))
+rownames(ro.omega) <- colnames(tvpars)
+colnames(ro.omega) <- colnames(tvpars)
+ro.omega
+ro.omega[2,1] <- ro.omega[1,2] <- .3   ## ka kt
+ro.omega[3,6] <- ro.omega[6,3] <- .3     ## CL VC
+ro.omega[3,8] <- ro.omega[8,3] <- .3     ## CL Q
+ro.omega[6,7] <- ro.omega[7,6] <- .4   ## VP VC
+## ro.omega
+
+omega <- sqrt(diag.omega) %*% t(sqrt(diag.omega)) * ro.omega
+## heatmap(omega)
 
 library(MASS)
 set.seed(9854934)
@@ -74,6 +96,12 @@ df.doses
 ###} Section end
 
 ###{ Section start: simulate observation times
+NTIM.obs <- c(seq(0,by=24*7,to=34*24),seq(816,by=4,to=816+48),816+24*7)
+
+
+df.obs <- merge(data.frame(ID=df.cov$ID),data.frame(NTIM=NTIM.obs))
+## noise has to be much larger on sparse samples than on rich profile
+df.obs$TIME <- df.obs$NTIM
 
 ###} Section end
 
@@ -89,7 +117,7 @@ df.doses
 pk1 <- PKPDsim::new_ode_model(code ="
 dAdt[1] = -KA1*A[1]
 dAdt[2] = KA1*A[1]-KT1*A[2] 
-dAdt[3] = KT1 * A[2] - CL*A[3]/VC - A[2]/VC*VM/(KM+A[2]/VC)
+dAdt[3] = KT1 * A[2] + Q*(A[4]/VP-A[3]/VC) - CL*A[3]/VC - A[2]/VC*VM/(KM+A[2]/VC)
 dAdt[4] = Q*(A[3]/VC-A[4]/VP)
 "
 ,dose = list(cmt = 1, bioav = "F1") ### param goes here
@@ -110,6 +138,7 @@ pkpdSimMany <- function(model,doses,pars,simtimes,col.id,debug=F,...){
     
    
     simres <- do.call(rbind,lapply(1:nrow(pars),function(row.pars){
+        id.row <- pars[row.pars,col.id]
         
         doses <- transform(doses[,c("times","amt")]
                           ,cmt=1
@@ -123,7 +152,7 @@ pkpdSimMany <- function(model,doses,pars,simtimes,col.id,debug=F,...){
                               ode = model
                               ,parameters = pars[row.pars,,drop=F]
                               ,regimen = regimen
-                              ,t_obs = simtimes
+                              ,t_obs = simtimes[simtimes[,col.id]==id.row,"TIME"]
                               ,...
                              ## ,int_step=.001
                           )
@@ -133,23 +162,35 @@ pkpdSimMany <- function(model,doses,pars,simtimes,col.id,debug=F,...){
         if(any(!is.finite(s.row[,"y"]))) warning(paste("Numerical issue for subject: ",col.id,"=",unique(s.row[,col.id]),". Try reducing t_sim?"))
         s.row
     }))
-
+    if(col.id!="id") simres <- subset(simres,select=-id)
+    
     simres
     
 }
 
-df.doses$amt <- 200
+df.doses$amt <- 500
 df.doses$times <-  df.doses$TIME
-sim1 <- pkpdSimMany(pk1,df.doses,pars=pars,simtimes=seq(0,by=4,to=8*7*24),col.id="ID")
+## sim1 <- pkpdSimMany(pk1,df.doses,pars=pars,simtimes=seq(0,by=4,to=8*7*24),col.id="ID")
+sim1 <- pkpdSimMany(pk1,df.doses,pars=pars,simtimes=df.obs,col.id="ID",debug=F)
 head(sim1)
 
 p1 <- ggplot(sim1,aes(t/24/7,y,group=ID))+geom_line()+facet_wrap(~comp,scales="free")
+
 p1
-p1+scale_y_log10()
+p1+geom_point()+scale_y_log10(limits=c(5,5e4))
+
 
 ## subset samples from simpulation times.
+## ggplot(subset(sim1,as.numeric(ID)<=5),aes(t/24/7,y,group=ID))+geom_line()+
+##     facet_wrap(~comp)+
+##     scale_y_log10(limits=c(10,5e4))+
+##     geom_point(data=subset(sim1,as.numeric(ID)<=5&t%in%c(seq(0,by=24*7,to=8*7*24),seq(816,by=4,to=816+48),816+24*3)),colour="red")
+
 
 ## observation noise
+
+## LLOQ
+
 ## miss samples at random
 
 ###} Section end
